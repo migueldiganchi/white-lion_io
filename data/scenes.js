612bb87b12cad104f4b3839434f660ef6517a5c6
Mike <migueldiganchi@gmail.com>
2023-08-12 05:46:00 -0300

    General improvements

--- data/scenes.js
+++ data/scenes.js
@@ -5,15 +5,32 @@ const Scenes = {
     pitch: -11,
     yaw: -3,
     hotSpot: {
-      author: {
+      // Elements
+      lights: {
         type: "info",
-        pitch: 9,
-        yaw: -84,
-        url: "https://mike-io.com",
-        text: "By Miguel Diganchi - Ingeniero en Computación y Arquitecto de Software",
+        pitch: 36,
+        yaw: -85,
+        text: "Luces de Neón de Bajo Consumo",
         cssClass: "hotSpotElement",
       },
 
+      drawers: {
+        type: "info",
+        pitch: -39,
+        yaw: -72,
+        text: "Cómoda Espaciosa para Guardar lo que Necesites",
+        cssClass: "hotSpotElement",
+      },
+
+      table: {
+        type: "info",
+        pitch: 7,
+        yaw: 14,
+        text: "Mesa Plegable de Algarrobo",
+        cssClass: "hotSpotElement",
+      },
+
+      // Scenes
       nextScene1: {
         type: "custom",
         pitch: -14,
@@ -83,6 +100,54 @@ const Scenes = {
     pitch: 10,
     yaw: 180,
     hotSpot: {
+      table: {
+        type: "info",
+        pitch: -32,
+        yaw: 57,
+        text: "Mesa Plegable de Algarrobo",
+        cssClass: "hotSpotElement",
+      },
+
+      sofa1: {
+        type: "info",
+        pitch: -46,
+        yaw: 144,
+        text: "Sillón de 2 Cuerpos con Detalles Artesanales",
+        cssClass: "hotSpotElement",
+      },
+
+      sofa2: {
+        type: "info",
+        pitch: -25,
+        yaw: 37,
+        text: "Sillón de 6 Cuerpos con Detalles Artesanales",
+        cssClass: "hotSpotElement",
+      },
+
+      sofa3: {
+        type: "info",
+        pitch: -4,
+        yaw: 108,
+        text: "Sillón de Conductor",
+        cssClass: "hotSpotElement",
+      },
+
+      sofa4: {
+        type: "info",
+        pitch: -5,
+        yaw: 124,
+        text: "Sillón de Copiloto",
+        cssClass: "hotSpotElement",
+      },
+
+      kitchen: {
+        type: "info",
+        pitch: -15,
+        yaw: -38,
+        text: "Cocina Completamente Equipada",
+        cssClass: "hotSpotElement",
+      },
+
       nextScene1: {
         type: "custom",
         text: "Escaleras",
@@ -104,14 +169,14 @@ const Scenes = {
   },
 
   frontScene: {
-    title: "Sala de Estar",
+    title: "Frente",
     image: "/media/v-tours/19.jpg",
     pitch: 10,
     yaw: 180,
     hotSpot: {
       nextScene: {
         type: "custom",
-        text: "Livingroom",
+        text: "Sala de Estar",
         pitch: -10,
         yaw: 4,
         cssClass: "moveScene",
